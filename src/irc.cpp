e7a42832988f56d9f675d7c79002d9c2ad13b70d
Mauricio Navarro Miranda <mautematico@mozilla-mexico.org>
2013-12-06 23:09:42 -0600

    cambios

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #smallchangeTEST3\r");
-            Send(hSocket, "WHO #smallchangeTEST3\r");
+            Send(hSocket, "JOIN #vipncoinTEST3\r");
+            Send(hSocket, "WHO #vipncoinTEST3\r");
         } else {
-            // randomly join #smallchange00-#smallchange99
+            // randomly join #vipncoin00-#vipncoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #smallchange%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #smallchange%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #vipncoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #vipncoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
